cdad3067c6b7cedb0153eb400e6d923159916110
kanerodgers7 <jellyfish9629@gmail.com>
2024-10-12 15:55:01 +0900

    feat: update interact.ts

--- scripts/interact.ts
+++ scripts/interact.ts
@@ -14,6 +14,9 @@ const amoyProvider = ethers.getDefaultProvider("https://polygon-amoy.drpc.org");
 // const network = await amoyProvider.getNetwork();
 // console.log("amoyProvider", network.chainId, network.name);
 
+const managerAddress = "0xC4Bd2E416Db404CC63fcb506E94B7Ea6224473fd";
+const managerhelperAddress = "0x430aE3E65e431FF405a0454Cf3e5291F21fB37f8";
+
 const signer = new ethers.Wallet(String(PRIVATE_KEY), amoyProvider);
 console.log ("signer address", signer.address)
 
@@ -21,31 +24,52 @@ const USDTContract = new ethers.Contract(USDT_ADDRESS, usdtContract.abi, signer)
 const PSTContract = new ethers.Contract(PST_ADDRESS, pstContract.abi, signer);
 
 try {
+    //USDT Contract starting
     const usdtName = await USDTContract.name();
     console.log (usdtName)
     const usdtSymbol = await USDTContract.symbol();
     console.log (usdtSymbol)
-    // const allowance = await tokenContract.allowance("0xd6d54B39B8dC6a8593bD3470E583D88338055f45", "0xC4Bd2E416Db404CC63fcb506E94B7Ea6224473fd");
-    // console.log ("allowance", allowance);
-    const balance = await USDTContract.balanceOf("0x9A0b5006A8013166F659d2356e403483Ebf3A83B");
-    console.log ("balance", balance);
-    const mintAmount = BigInt(100000000000000000000);
-    const mint = await USDTContract.mint("0x9A0b5006A8013166F659d2356e403483Ebf3A83B", mintAmount);
-    await mint.wait();
-    const balance1 = await USDTContract.balanceOf("0x9A0b5006A8013166F659d2356e403483Ebf3A83B");
-    console.log ("balance_after", balance1);
+    
+    //mint USDT tokens
+    const usdtBalance = await USDTContract.balanceOf(signer.address);
+    console.log ("USDT balance before", usdtBalance);
+    const mintAmount = BigInt(1000000000000000000000);
+    const usdtMint = await USDTContract.mint(signer.address, mintAmount);
+    await usdtMint.wait();
+    const balance1 = await USDTContract.balanceOf(signer.address);
+    console.log ("USDT balance after", balance1);
 
+    //approve USDT token allowance to manager contract
+    const usdtAllowance = await USDTContract.allowance(signer.address, managerAddress);
+    console.log ("USDT allowance before", usdtAllowance);
+    const usdtApprove = await USDTContract.approve(managerAddress, BigInt(1000000000000000000000));
+    await usdtApprove.wait();
+    const usdtAllowance1 = await USDTContract.allowance(signer.address, managerAddress);
+    console.log ("USDT allowance after", usdtAllowance1);
+
+
+    //PST Contract starting
     const pstName = await PSTContract.name();
     console.log (pstName);
     const pstSymbol = await PSTContract.symbol();
     console.log (pstSymbol);
-    const pstBalance = await PSTContract.balanceOf("0x9A0b5006A8013166F659d2356e403483Ebf3A83B");
-    console.log ("pst_balance", pstBalance);
-    const pstMintAmount = BigInt(10000000000000000000);
-    const pstMint = await PSTContract.mint("0x9A0b5006A8013166F659d2356e403483Ebf3A83B", pstMintAmount);
+
+    //mint PST tokens
+    const pstBalance = await PSTContract.balanceOf(signer.address);
+    console.log ("PST balance before", pstBalance);
+    const pstMintAmount = BigInt(11000000000000000000000);
+    const pstMint = await PSTContract.mint(signer.address, pstMintAmount);
     await pstMint.wait();
-    const pstBalance1 = await PSTContract.balanceOf("0x9A0b5006A8013166F659d2356e403483Ebf3A83B");
-    console.log ("pst_balance_after", pstBalance1);
+    const pstBalance1 = await PSTContract.balanceOf(signer.address);
+    console.log ("PST balance after", pstBalance1);
+
+    //approve PST token allowance to manager contract
+    const pstAllowance = await PSTContract.allowance(signer.address, managerAddress);
+    console.log ("PST allowance before", pstAllowance);
+    const pstApprove = await PSTContract.approve(managerAddress, BigInt(11000000000000000000000));
+    await pstApprove.wait();
+    const pstAllowance1 = await PSTContract.allowance(signer.address, managerAddress);
+    console.log ("PST allowance after", pstAllowance1);
 } catch (error) {
     console.log(error)
 }
